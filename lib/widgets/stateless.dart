1e3ffaf3c98f4bd62a2bd11bd03789bd39a61b76
MuhammedHalid <91598377+MuhammedHalid@users.noreply.github.com>
2024-01-27 23:21:23 +0530

    n

--- lib/widgets/stateless.dart
+++ lib/widgets/stateless.dart
@@ -8,9 +8,11 @@ class Stateless extends StatelessWidget {
     return const Scaffold(
       body: SingleChildScrollView(
         child: Column(children: [
+          Text("Stateless Widget"),
           Text('A widget that does not require mutable state. The StatelessWidget does not have any state information. It remains static throughout its lifecycle. We use a stateless widget when we create an application that does not require redrawing a widget again and again. The examples of the StatelessWidget are Text, Row, Column, Container, etc.  ')
         ]),
       ),
     );
   }
 }
+
