a62ffd1b6be78ead6df36cb5008d43796d9fadec
MuhammedHalid <91598377+MuhammedHalid@users.noreply.github.com>
2024-01-28 23:01:40 +0530

    n

--- lib/widgets/stateless.dart
+++ lib/widgets/stateless.dart
@@ -9,7 +9,13 @@ class Stateless extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(children: [
           Text("Stateless Widget"),
-          Text('A widget that does not require mutable state. The StatelessWidget does not have any state information. It remains static throughout its lifecycle. We use a stateless widget when we create an application that does not require redrawing a widget again and again. The examples of the StatelessWidget are Text, Row, Column, Container, etc.  ')
+          SizedBox(height: 10),
+
+          Text('A widget that does not require mutable state. The StatelessWidget does not have any state information. It remains static throughout its lifecycle. We use a stateless widget when we create an application that does not require redrawing a widget again and again. The examples of the StatelessWidget are Text, Row, Column, Container, etc.  '),
+          Text("Stateful Widget"),
+
+          SizedBox(height: 10),
+          Text('The widgets whose state can be altered once they are built are called stateful Widgets. These states are mutable and can be changed multiple times in their lifetime. This simply means the state of an app can change multiple times with different sets of variables, inputs, data.This widget does not have a build() method. It has createState() method, which returns a class that extends the Flutters State Class. The examples of the StatefulWidget are Checkbox, Radio, Slider, InkWell, Form, and TextField.'),
         ]),
       ),
     );
