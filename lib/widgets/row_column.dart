dff5cd9f21722ca0e668186d7fabb545a49738e4
MuhammedHalid <91598377+MuhammedHalid@users.noreply.github.com>
2024-02-05 22:31:06 +0530

    t

---
+++ lib/widgets/row_column.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RowColumn extends StatefulWidget {
+  const RowColumn({super.key});
+
+  @override
+  State<RowColumn> createState() => _RowColumnState();
+}
+
+class _RowColumnState extends State<RowColumn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(),
+    );
+  }
+}
